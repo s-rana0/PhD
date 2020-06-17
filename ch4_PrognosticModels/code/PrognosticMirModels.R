8cd15582616d2413ae6e78b96586f9dde7eb6ae4
Sharmila <sharmila.rana11@hotmail.com>
2020-06-17 20:15:45 +0100

    Added new directory for chapter 4

---
+++ ch4_PrognosticModels/code/PrognosticMirModels.R
@@ -0,0 +1,6 @@
+# Code for PM 190926_TCGA_isomiRs.R
+# For partitioning dataset 190904_filtering_TCGA.R
+# For TCGA isomiR functions 190806_TCGA_isomiRs_functions.R
+
+# TO DO:
+# 1) Update the phenotype metadata with additional samples + PSA info!
